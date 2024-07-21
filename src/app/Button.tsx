1dbfaf71def8af3103e3dadc53b71f8f19bc3449
sudhanshuw360 <139438414+sudhanshuw360@users.noreply.github.com>
2024-07-21 20:13:52 +0530

    fix: added workflow

--- src/app/Button.tsx
+++ src/app/Button.tsx
@@ -46,7 +46,7 @@ export const Button = ({
         )}
         {...props}
       >
-        {label}
+        {label}fsdafsf
         <style jsx>{`
           button {
             background-color: ${backgroundColor};
